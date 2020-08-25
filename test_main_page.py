be33f6125e77d8bbde5e33c5c4c1223b8480a93d
Ilya Borodin <rangorot@gmail.com>
2020-08-25 18:59:52 +0400

    Добавлены изменения, которые позволяют обращаться к элемент через локаторы, прописанные в отдельном файле locators

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
+    # ссылка с ошибкой http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
